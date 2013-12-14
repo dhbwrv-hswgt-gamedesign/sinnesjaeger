477a05887e56322de62c38202d713657f3818a8b
Branimir Djordjevic <branimir.djordjevic@gmail.com>
2013-12-14 12:49:36 +0100

    added NFC class

--- src/de/hrw/zoo/activity/MainActivity.java
+++ src/de/hrw/zoo/activity/MainActivity.java
@@ -46,6 +46,7 @@ import de.hrw.zoo.dialog.LoginDialog;
 import de.hrw.zoo.list.PlayerList;
 import de.hrw.zoo.listener.OnZooItemSelectedListener;
 import de.hrw.zoo.model.Player;
+import de.hrw.zoo.nfc.reader.NdefReaderTask;
 
 public class MainActivity extends Activity {
 	
@@ -56,7 +57,6 @@ public class MainActivity extends Activity {
 	private SelectionWheel mWheel;
     public static final String MIME_TEXT_PLAIN = "text/plain";
     public static final String TAG = "ZooApp";
-    private TextView mTextView;
     private NfcAdapter mNfcAdapter;
 
 	@Override
@@ -169,6 +169,26 @@ public class MainActivity extends Activity {
         }
 		
 	}
+	
+    private void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+    	
+	  	final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TEXT_PLAIN);
+        } catch (MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+	
+}
 
 	public void onContentChanged() {
 		super.onContentChanged();
@@ -187,26 +207,6 @@ public class MainActivity extends Activity {
          */
         setupForegroundDispatch(this, mNfcAdapter);
     }
-    private void setupForegroundDispatch(final Activity activity,
-			NfcAdapter adapter) {
-    	
-    	  	final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
-	        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-	        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
-	        IntentFilter[] filters = new IntentFilter[1];
-	        String[][] techList = new String[][]{};
-	        // Notice that this is the same filter as in our manifest.
-	        filters[0] = new IntentFilter();
-	        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
-	        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
-	        try {
-	            filters[0].addDataType(MIME_TEXT_PLAIN);
-	        } catch (MalformedMimeTypeException e) {
-	            throw new RuntimeException("Check your mime type.");
-	        }
-	        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
-		
-	}
 
 	@Override
     protected void onPause() {
@@ -222,61 +222,6 @@ public class MainActivity extends Activity {
 		
 	}
     
-    
-    private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
-        @Override
-        protected String doInBackground(Tag... params) {
-            Tag tag = params[0];
-            Ndef ndef = Ndef.get(tag);
-            if (ndef == null) {
-                // NDEF is not supported by this Tag.
-                return null;
-            }
-            NdefMessage ndefMessage = ndef.getCachedNdefMessage();
-            NdefRecord[] records = ndefMessage.getRecords();
-            for (NdefRecord ndefRecord : records) {
-                if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
-                    try {
-                        return readText(ndefRecord);
-                    } catch (UnsupportedEncodingException e) {
-                        Log.e(TAG, "Unsupported Encoding", e);
-                    }
-                }
-            }
-            return null;
-        }
-        private String readText(NdefRecord record) throws UnsupportedEncodingException {
-            /*
-             * See NFC forum specification for "Text Record Type Definition" at 3.2.1
-             *
-             * http://www.nfc-forum.org/specs/
-             *
-             * bit_7 defines encoding
-             * bit_6 reserved for future use, must be 0
-             * bit_5..0 length of IANA language code
-             */
-            byte[] payload = record.getPayload();
-            // Get the Text Encoding
-            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
-            // Get the Language Code
-            int languageCodeLength = payload[0] & 0063;
-            // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
-            // e.g. "en"
-            // Get the Text
-            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
-        }
-        @Override
-        protected void onPostExecute(String result) {
-            if (result != null) {
-            	Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG).show();
-            	Player player = new Player(result);
-            	PlayerList list = new PlayerList();
-            	//View view = getLayoutInflater().inflate(R.layout.fragment_login, null);
-            	//inal LoginDialog dlg = new LoginDialog(getBaseContext(), view, mAppCenter)
-            	
-            }
-        }
-    }
 
 	@Override
     protected void onNewIntent(Intent intent) {
